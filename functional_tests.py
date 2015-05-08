50b7a2d8030bb74f83f3ac8e75f501cd25bf7a4c
Ryan Camaratta <ryancnap@gmail.com>
2015-05-08 14:13:22 -0400

    functional tests now using unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,20 +1,31 @@
 #! /usr/bin/env python
 
-from selenium import webdriver
-
-# this section added to satisfy Travis
-# enabless headless firefox
-# pyvirtualdisplay is a python wrapper for xvfb
 from pyvirtualdisplay import Display
+from selenium import webdriver
+import unittest
 
-display = Display(visible = 0, size=(800, 600))
-display.start()
+class NewVisitorTest(unittest.TestCase):
 
-browser = webdriver.Firefox()
-browser.get('http://www.google.com')
+	def setUp(self):
+		display = Display(visible = 0, size=(800, 600))
+		display.start()
 
-assert 'Google' in browser.title
+		self.browser = webdriver.Firefox()
+		self.browser.implicitly_wait(3)
 
-# always fill your divits
-browser.close()
-display.stop()
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		# You hear about an online app and go check it's homepage.
+		self.browser.get('http://localhost:8000')
+
+		# You notice the page title and header mention todo lists.
+		self.assertIn('To-Do', self.browser.title)
+		self.fail('Remember to finish this test.')
+
+		# You are invited to enter a to-do item straight away.
+		# rest of test, comments, etc.
+
+if __name__ == '__main__':
+	unittest.main()
