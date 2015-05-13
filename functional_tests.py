1896a569429f7e3b3d058c109d69b9b5a8190b66
Ryan Camaratta <ryancnap@gmail.com>
2015-05-13 13:30:58 -0400

    refine functional test a wee bit

--- functional_tests.py
+++ functional_tests.py
@@ -20,6 +20,12 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	# Add a helper method; only methods starting with _test will be run.
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# You hear about an online app and go check it's homepage.
 		self.browser.get('http://localhost:8000')
@@ -44,23 +50,17 @@ class NewVisitorTest(unittest.TestCase):
 		# When you hit enter, the page updates and now lists
 		# 1: Buy peacock feathers as an item in a to-do list table.
 		inputbox.send_keys(Keys.ENTER)
-
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		# There is still a text box inviting you to add another item.
+		# You enter 'Use peacock feathers to make a fly'
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
 
 		# The page updates again, and now shows both items on the list.
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-		self.assertIn('2: Use peacock feathers to make a fly',
-			[row.text for row in rows]
-		)
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 		# You wonder whether the site will remember your list. Then you see
 		# that the site has generated a unique url for you.
