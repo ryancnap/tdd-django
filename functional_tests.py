9de7c1a2daad93eaf6c9ada22893d674c68f5a0e
Ryan Camaratta <ryancnap@gmail.com>
2015-05-13 13:17:04 -0400

    intentionally break functional tests to allow adding more functionality to adding to-do list items

--- functional_tests.py
+++ functional_tests.py
@@ -50,6 +50,21 @@ class NewVisitorTest(unittest.TestCase):
 		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
 		# There is still a text box inviting you to add another item.
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
+
+		# The page updates again, and now shows both items on the list.
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.assertIn('2: Use peacock feathers to make a fly',
+			[row.text for row in rows]
+		)
+
+		# You wonder whether the site will remember your list. Then you see
+		# that the site has generated a unique url for you.
+
 		self.fail('Finish the test!')
 
 if __name__ == '__main__':
